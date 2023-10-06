402865f6eec85a458a5eed3b454f51bd609f8765
sontekliu <sontek@yeah.net>
2023-10-06 11:56:46 +0800

    change float term

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,22 +1,22 @@
-return {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-        require("toggleterm").setup{}
-    end,
-    keys = {
-        { '<leader>ft',       "<cmd>:ToggleTerm direction='float'<CR>", { desc = '打开终端' } },
-        -- { '<leader>ftt',       "<cmd>:ToggleTerm direction='tab'<CR>", { desc = '打开终端' } },
-        -- { '<leader>ftv',       "<cmd>:ToggleTerm direction='vertical'<CR>", { desc = '打开终端' } },
-        -- { '<leader>fth',       "<cmd>:ToggleTerm direction='horizontal'<CR>", { desc = '打开终端' } },
-    }
-}
-
-
 -- return {
---     "voldikss/vim-floaterm",
+--     "akinsho/toggleterm.nvim",
+--     version = "*",
+--     config = function()
+--         require("toggleterm").setup{}
+--     end,
 --     keys = {
---         { '<leader>ft',       "<cmd>:FloatermToggle<CR>", { desc = '打开终端' } },
+--         { '<leader>ft',       "<cmd>:ToggleTerm direction='float'<CR>", { desc = '打开终端' } },
+--         -- { '<leader>ftt',       "<cmd>:ToggleTerm direction='tab'<CR>", { desc = '打开终端' } },
+--         -- { '<leader>ftv',       "<cmd>:ToggleTerm direction='vertical'<CR>", { desc = '打开终端' } },
+--         -- { '<leader>fth',       "<cmd>:ToggleTerm direction='horizontal'<CR>", { desc = '打开终端' } },
 --     }
 -- }
+--
+
+return {
+    "voldikss/vim-floaterm",
+    keys = {
+        { '<leader>ft',       "<cmd>:FloatermToggle<CR>", { desc = '打开终端' } },
+    }
+}
 
